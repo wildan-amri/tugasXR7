a7d24ef5c6284f660e955e6662495f53306a0890
Wildan <mohammad_amri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:39:12 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,13 @@
+public class data {
+    public static void main(String[] args) {
+        String nama ="wildan";
+        int usia = 16;
+        String alamat = "Gurah,Kediri";
+        double kelas = 10.7;
+
+        System.out.println("nama saya : " + nama);
+        System.out.println("asal saya : " + alamat);
+        System.out.println("usia saya : " + usia + " tahun");
+        System.out.println("saya kelas : " + kelas);
+    }
+}
