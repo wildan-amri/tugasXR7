3b819b73fecc67af8546b41cd3663c9b518eb1ec
Wildan <mohammad_amri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 13:47:10 +0700

    Add files via upload

--- siswatester.java
+++ siswatester.java
@@ -1,37 +1,31 @@
 //Driver CLass
+import java.util.Scanner;
 public class siswatester {
-
+    
     public static void main(String[] args) {
-        siswa erwan = new siswa();
-        siswa arjuna = new siswa();
-        siswa bima = new siswa();
-        siswa melvin = new siswa();
-        siswa azzam = new siswa();
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukan nama siswa : ");
+        String nama = input.nextLine();
+        System.out.println("masukan id siswa : ");
+        int id = input.nextInt();
+        System.out.println("masukan IPK siswa : ");
+        double ipk = input.nextDouble();
 
-        erwan.id = 8;
-        erwan.nama ="erwan";
-        erwan.ipk = 90;
+        siswa wildan = new siswa(id, nama, ipk);
+        wildan.print();
+        siswa erwan = new siswa(8, "erwan", 90);
+         siswa arjuna = new siswa(1, "arjuna", 90);
+         siswa bima = new siswa(2, "bima", 90);
+         siswa melvin = new siswa(21, "melvin", 90);
+         siswa azzam = new siswa(22, "azam", 90);
 
-        arjuna.id = 1;
-        arjuna.nama ="arjuna";
-        arjuna.ipk = 90;
+        
 
-        bima.id = 2;
-        bima.nama ="bima";
-        bima.ipk = 90;
-
-        melvin.id = 21;
-        melvin.nama ="melvin";
-        melvin.ipk = 90;
-
-        azzam.id = 22;
-        azzam.nama ="azzam";
-        azzam.ipk = 90;
-
-        System.out.println("nama:"+erwan.nama+" absen:"+erwan.id+" ipk:"+erwan.ipk);
-        System.out.println("nama:"+arjuna.nama+" absen:"+arjuna.id+" ipk:"+arjuna.ipk);
-        System.out.println("nama:"+bima.nama+" absen:"+bima.id+" ipk:"+bima.ipk);
-        System.out.println("nama:"+melvin.nama+" absen:"+melvin.id+" ipk:"+melvin.ipk);
-        System.out.println("nama:"+azzam.nama+" absen:"+azzam.id+" ipk:"+azzam.ipk);
+         erwan.print();
+         arjuna.print();
+         bima.print();
+         melvin.print();
+         azzam.print();
+         input.close();
     }
 }
