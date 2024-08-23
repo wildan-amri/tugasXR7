241bdabe6f553c71cf15dbfdeea9bb90283ddfcb
Wildan <mohammad_amri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 13:57:53 +0700

    Add files via upload

--- karyawantester.java
+++ karyawantester.java
@@ -1,5 +1,19 @@
+  import java.util.Scanner;
 public class karyawantester {
     public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("masukan id");
+      int id = input.nextInt();
+      System.out.println("masukan nama karyawan");
+      String nama = input.nextLine();
+      System.out.println("masukan unit karyawan");
+      String unit = input.nextLine();
+      System.out.println("masukan alamat karyawan");
+      String alamat = input.nextLine();
+
+      karyawan erwan = new karyawan(id, nama, unit, alamat);
+      erwan.print();
+      System.out.println();
         karyawan angger = new karyawan(2, "annger", "kurikulum", "malang");
         karyawan cya = new karyawan(3, "cya", "tukang kebersihan", "madura");
         karyawan zitta = new karyawan(4, "zitta", "tenaga usaha", "nganjuk");
@@ -11,5 +25,7 @@ public class karyawantester {
       zitta.print();
       wildan.print();
       abid.print();
+      input.close();
+
     }
 }
